adddf9fef78de08a285ccd4dae6742e63f878b8b
Flozerty <floris.louerat@gmail.com>
2024-02-28 14:46:20 +0100

    exo11

---
+++ part1/exo11.php
@@ -0,0 +1,21 @@
+<h1>Exercice 6</h1>
+<p><b>
+    Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce
+    tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé
+    d’afficher le nombre de marques de voitures présentes dans le tableau.
+  </b></p>
+
+<?php 
+$tableau = ["Peugeot", "Renault", "BMW", "Mercedes"];
+
+function afficher($table) {
+
+  echo"Il y a ".count($table)." marques de voitures dans le tableau :<br>";
+  echo"<ul>";
+  foreach ($table as $marque) {
+      echo "<li>$marque</li>";
+  }
+  echo"</ul>";
+}
+
+afficher($tableau);
