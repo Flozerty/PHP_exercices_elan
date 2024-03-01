04d09c96ebe5d2438475710d66a23f84122b8018
Flozerty <floris.louerat@gmail.com>
2024-03-01 19:43:28 +0100

    exo12

---
+++ part2/exo12.php
@@ -0,0 +1,34 @@
+<h1>Exercice 12</h1>
+<b>
+  <p>
+    La fonction var_dump($variable) permet d’afficher les informations d’une variable.
+    Soit le tableau suivant : <br>
+    $tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));<br>
+    A l’aide d’une boucle, afficher les informations des variables contenues dans le tableau.
+  </p>
+</b>
+
+<?php
+
+$tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
+
+foreach($tableauValeurs as $value) {
+var_dump($value);
+echo"<br>";
+}
+
+echo"<br>";
+
+for($i=0; $i<count($tableauValeurs); $i++) {
+var_dump($tableauValeurs[$i]);
+echo"<br>";
+}
+
+echo"<br>";
+
+$i=0;
+while ($i<count($tableauValeurs)) {
+var_dump($tableauValeurs[$i]);
+echo"<br>";
+$i++;
+}
