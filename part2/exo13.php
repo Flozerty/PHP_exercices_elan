9dd46a93a86538ae38782650c2b9c07b5947ed9e
Flozerty <floris.louerat@gmail.com>
2024-03-03 15:57:25 +0100

    exo13 fun

--- part2/exo13.php
+++ part2/exo13.php
@@ -33,17 +33,45 @@ main {
 
 #board {
   background-color: rgb(230, 230, 230);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+}
+
+#board ul {
+
+  max-height: 350px;
+  overflow-y: auto;
+}
+
+#vInfoStatus {
+  color: grey;
 }
 
 #vehiclesBoard {
   background-color: rgb(230, 230, 230);
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  flex-wrap: wrap;
   height: fit-content;
+  max-width: 60%;
+}
+
+.vInfo {
+  border: 2px dashed black;
+  width: 45%;
+}
+
+.vInfo:nth-child(n+4),
+.vInfo:nth-child(n+3) {
+  border-top: none;
+  width: 45%;
 }
 
 .vInfo p {
   margin: 5px;
+
 }
 
 ul {
@@ -161,12 +189,17 @@ class Voiture {
 
   public function ralentir($speed){
     if($this->getPower()) {
-      if ($this->vitesseActuelle < $speed) {
+      if($this->vitesseActuelle==0){
+        //vitesse = 0
+        return"Le véhicule $this->marque $this->model ne peut pas ralentir de $speed km/h,
+        Il n'est pas encore parti!";
+
+      } elseif ($this->vitesseActuelle < $speed) {
         // ne peut pas ralentir plus
         $this->setVitesseActuelle(0);
-        return"
-        Le véhicule $this->marque $this->model veut ralentir de $speed km/h.<br>
+        return"Le véhicule $this->marque $this->model veut ralentir de $speed km/h.<br>
         Il atteint 0 km/h et est maintenant à l'arrêt.";
+
       } else {
         // ralentit normalement
         $this->vitesseActuelle -= $speed;
@@ -184,24 +217,39 @@ class Voiture {
     return "
       <p>Nom et modèle du véhicule :<br> $this->marque $this->model</p>
       <p>Nombre de portes : $this->nbportes</p>
-      <p>Le véhicule $this->marque est ".($this->power ? "démarré" : "à l'arrêt")."</p>
+      <p>Le véhicule $this->marque est ".($this->power ? "démarré" : "éteint")."</p>
       <p>Sa vitesse actuelle est de $this->vitesseActuelle km/h</p>";
   }
 }
                             ///////////////////////////////////
-                            /////////// VARIABLES /////////////
+                            ///////////////////////////////////
                             ///////////////////////////////////
 $vehicules = [
 $v1 = new Voiture("peugeot", "408", 5),
 $v2 = new Voiture("citroën", "C4", 3),
+$v3 = new Voiture("peugeot", "508", 4),
+$v4 = new Voiture("toyota", "Aygo X", 5),
+$v5 = new Voiture("opel", "corsa", 3),
+$v6 = new Voiture("audi", "A5", 3),
 ];
 
 $instructions = [
   $v1->demarrer(),
   $v1->accelerer(50),
-  $v2->demarrer(),
-  $v2->stopper(),
+  $v4->demarrer(),
+  $v5->stopper(),
   $v2->accelerer(20),
+  $v6->demarrer(),
+  $v6->accelerer(50),
+  $v6->ralentir(50),
+  $v1->demarrer(),
+  $v6->stopper(),
+  $v2->accelerer(20),
+  $v4->ralentir(100),
+  $v1->accelerer(50),
+  $v2->demarrer(),
+  $v3->stopper(),
+  $v4->accelerer(20),
 ];
 
 function displayVehicles($vehicules) {
@@ -224,15 +272,28 @@ function displayVehicles($vehicules) {
 
 function displayBoard($instructions, $vehicules) {
   $result = "<div id='board'><ul>";
-  
+  //affichage des instructions
   foreach($instructions as $instruction) {
     $result .= "<li>$instruction</li>";
   }
-
+  
+  // affichage du rendu final des véhicules :
+  $result .= "</ul><div id='vInfoStatus'>";
   foreach($vehicules as $v) {
-    $result .= "La vitesse du véhicule ".$v->getmarque()." ".$v->getmodel()." est de ".$v->getvitesseActuelle()." km/h<br>";
+
+    $result .= "Le véhicule ".$v->getmarque()." ".$v->getmodel();
+  //Si le véhicule roule
+    if ($v->getvitesseActuelle()>0) { 
+      $result .= " roule à ".$v->getvitesseActuelle()." km/h";
+    } else {
+
+  //Si véhicule éteint, Power ? On : Off 
+      $result .= ($v->getPower()) ?  " est à l'arret" : " est eteint";
+    }
+    $result .="<br>";
   }
-   $result .= "</ul></div>";
+  
+  $result .= "</div></div>";
   return $result;
 }
 
